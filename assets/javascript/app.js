6523c84f8edb5c015070cfe26c78748f92a4d59b
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-19 07:28:55 -0600

    Attempts some styling with Materialize framework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,15 @@ $(document).ready(function() {
         full_width: true
     }); //slider init
 
+    // Next slide
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', 3); // Move next n times.
+    // Previous slide
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', 4); // Move prev n times.
+    // Set to nth slide
+    $('.carousel').carousel('set', 4);
+
     var queryURL = "https://pixabay.com/api/videos/?key=3796838-b739bb7867b88767ec1631840&q=japan";
 
     $('#search').on('click', newWidget);
