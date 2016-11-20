21f1a3ec86095c432dcdad855c9b3a19accc31a9
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-20 09:33:44 -0600

    Adds progress bar.  Fixes a few loading issues.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
-
+    $('.progress').hide();
+    // Creates parallax effect
     $('.parallax').parallax();
 
+    // When button is pressed, grab the user input to start our search
 
     $('#search').on('click', function() {
-
+        $('.progress').show();
+        // API key and parameters
         var parameters = $('#location-input').val().trim();
         var pathway = 'https://www.googleapis.com/youtube/v3/search?part=snippet&safesearch=strict&maxResults=6&length=short';
         var apiKey = '&key=AIzaSyDcpRXHyPmdHfd_j7uIwLH5XZ1uk9V5E1k&q=';
 
         var queryURL = pathway + apiKey + 'travel' + parameters + '&type=video';
 
+        // Ajax call to access YouTube API
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).done(function(response) {
-            console.log(response);
 
+            // Empty out video div to prevent blank cards
+            $('.progress').hide();
+            $('#videos').empty();
+
+            // Loop through the API call to get Video IDs and descriptive paragraph.
             for (var i = 0; i < response.items.length; i++) {
 
                 $('#videos').append('<div class="col s12 m4" id="col-' + i + '">');
@@ -29,11 +37,13 @@ $(document).ready(function() {
             }
 
         });
+        // Prevent user from seeing ugly blank video players
+        $.blockUI;
+
+        //Empty the search field and keep results within the app
+        $('#location-input').val("");
         return false;
     })
-
-
-
 
 
 });
