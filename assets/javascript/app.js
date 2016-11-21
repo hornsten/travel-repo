7f9f05ed182c7947f6f6e46fcb0171f917b4b73b
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-20 20:41:02 -0600

    Ensures that all content is served over https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
                 $('#videos').append('<div class="col s12 m4" id="col-' + i + '">');
                 $('#col-' + i).append('<div class="card center medium" style="border-radius:2%" id="card-' + i + '">');
                 $('#card-' + i).append('<div class="video-container materialboxed" id="container-' + i + '">');
-                $('#container-' + i).append('<object id="object-' + i + '" width="400" height="300" data="http://www.youtube.com/embed/' + response.items[i].id.videoId + '?controls=0"></object>');
+                $('#container-' + i).append('<object id="object-' + i + '" width="400" height="300" data="https://www.youtube.com/embed/' + response.items[i].id.videoId + '?controls=0"></object>');
                 $('#card-' + i).append('<div class="card-content" id="content-' + i + '">');
                 $('#content-' + i).append('<p class="teal-text text-darken-3">' + response.items[i].snippet.description);
 
