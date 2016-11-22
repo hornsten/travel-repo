98d339bd16cb3b00dea0d685323741fd59948b83
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-21 22:54:38 -0600

    Displays rome2rio information in collections

--- assets/javascript/rome2rio.js
+++ assets/javascript/rome2rio.js
@@ -21,13 +21,14 @@ $(document).ready(function() {
             }).done(function(response) {
                 console.log(response);
                 for (var i = 0; i < response.routes.length; i++) {
-                    $('#travel-info').append('<ul class="collapsible" data-collapsible="accordion" id="ul-"' + i);
-                    $('#travel-info').append('<h4>' + "Route name: " + response.routes[i].name);
-                    $('#travel-info').append('<ul id="travel-stats-' + i + '">')
-                    $('#travel-stats-' + i).append('<li>' + "Distance: " + response.routes[i].distance + " miles");
-                    $('#travel-stats-' + i).append('<li>' + "Current price: $" + response.routes[i].indicativePrices["0"].price + " USD");
-                    $('#travel-stats-' + i).append('<li>' + "Price Range: $" + response.routes[i].indicativePrices["0"].priceLow + " - $" + response.routes["0"].indicativePrices["0"].priceHigh + " USD");
-                    $('#travel-stats-' + i).append('<li>' + "Total Duration: " + response.routes[i].totalDuration + " minutes");
+                    $('#travel-info').append('<div class="col s12 m6" id="info-' + i + '">');
+                    $('#info-' + i).append('<ul class="collection with-header" id="ul-' + i + '">');
+                    $('#ul-' + i).append('<li class="collection-header" id="' + i + '"><h4>Route name: ' + response.routes[i].name)
+                        .append('<ul id="travel-stats-' + i + '">')
+                        .append('<li class="collection-item" id="coll-' + i + '">Distance: ' + response.routes[i].distance + " miles")
+                        .append('<li class="collection-item" id="coll-' + i + '">Current price: $' + response.routes[i].indicativePrices["0"].price + " USD")
+                        .append('<li class="collection-item" id="coll-' + i + '">Price Range: $' + response.routes[i].indicativePrices["0"].priceLow + " - $" + response.routes["0"].indicativePrices["0"].priceHigh + " USD")
+                        .append('<li class="collection-item" id="coll-' + i + '">Total Duration: ' + response.routes[i].totalDuration + " minutes");
 
                 }
                 return false;
