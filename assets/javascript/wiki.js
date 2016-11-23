3da19bfba9b1c0043154a0b73cbd4cfdec1c6955
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-23 11:34:18 -0600

    Wiki info, travel informataion and videos are all able to display on index.html

--- assets/javascript/wiki.js
+++ assets/javascript/wiki.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
 
-    $.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Madrid&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function(data, textStatus, jqXHR) {
+    $('#search-travel-info').on('click', getPlaceInfo);
 
-            var markup = data.parse.text["*"];
-            var blurb = $('<div></div>').html(markup);
+    function getPlaceInfo() {
+        var place = $("#destination").val();
+        $.ajax({
+            type: "GET",
+            url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + place + "&callback=?",
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+            success: function(data, textStatus, jqXHR) {
 
-            // remove links as they will not work
-            blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+                var markup = data.parse.text["*"];
+                var blurb = $('<div id="blurb"></div>').html(markup);
 
-            // remove any references
-            blurb.find('sup').remove();
+                // remove links as they will not work
+                blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
 
-            // remove cite error
-            blurb.find('.mw-ext-cite-error').remove();
-            $('#article').html($(blurb).find('p'));
+                // remove any references
+                blurb.find('sup').remove();
 
-        },
-        error: function(errorMessage) {}
-    });
+                // remove cite error
+                blurb.find('.mw-ext-cite-error').remove();
+                $('#article').html($(blurb).find('p'));
+
+            },
+            error: function(errorMessage) {}
+        });
+
+    };
 });
