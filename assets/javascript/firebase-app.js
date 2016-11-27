2fe205b91c51e8fbada6f9af707b42f0c38f724f
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-27 09:25:56 -0600

    Users are now added to the database upon login or signup

--- assets/javascript/firebase-app.js
+++ assets/javascript/firebase-app.js
@@ -1,112 +1,123 @@
-//Initialize Firebase
+$(document).ready(function() {
+    //Initialize Firebase
 
-var config = {
-    apiKey: "AIzaSyDf9fnuCfrXVVBNQu4L2FTw_C1kLRrwZ1g",
-    authDomain: "travel-repo.firebaseapp.com",
-    databaseURL: "https://travel-repo.firebaseio.com",
-    storageBucket: "travel-repo.appspot.com",
-    messagingSenderId: "410921328144"
-};
+    var config = {
+        apiKey: "AIzaSyDf9fnuCfrXVVBNQu4L2FTw_C1kLRrwZ1g",
+        authDomain: "travel-repo.firebaseapp.com",
+        databaseURL: "https://travel-repo.firebaseio.com",
+        storageBucket: "travel-repo.appspot.com",
+        messagingSenderId: "410921328144"
+    };
 
-firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-var database = firebase.database().ref();;
-//Get Elements
-var txtEmail = $('#txtEmail');
-var txtPassword = $('#txtPassword');
-var btnLogin = $('#btnLogin');
-var btnSignUp = $('#btnSignUp');
-var btnLogout = $('#btnLogout');
+    var database = firebase.database().ref();
+    //Get Elements
+    var txtEmail = $('#txtEmail');
+    var txtPassword = $('#txtPassword');
+    var btnLogin = $('#btnLogin');
+    var btnSignUp = $('#btnSignUp');
+    var btnLogout = $('#btnLogout');
 
-btnLogin.show();
-btnSignUp.show();
-btnLogout.hide();
+    btnLogin.show();
+    btnSignUp.show();
+    btnLogout.hide();
 
-// Modal
-$('#modal1').modal('open');
-$('.modal').modal({
-    dismissible: true, // Modal can be dismissed by clicking outside of the modal
-    opacity: .5, // Opacity of modal background
-    in_duration: 300, // Transition in duration
-    out_duration: 200, // Transition out duration
-    starting_top: '4%', // Starting top style attribute
-    ending_top: '10%', // Ending top style attribute
-    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+    // Modal
+    $('#modal1').modal('open');
+    $('.modal').modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        starting_top: '4%', // Starting top style attribute
+        ending_top: '10%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
 
-        console.log(modal, trigger);
-    },
-    complete: function() {} // Callback for Modal close
-});
+            console.log(modal, trigger);
+        },
+        complete: function() {} // Callback for Modal close
+    });
 
-//Add login event
+    //Add login event
 
-btnLogin.on('click', function() {
+    btnLogin.on('click', function() {
 
-    //get email and pass
+        //get email and pass
 
-    var email = txtEmail.val().trim();
-    var pass = txtPassword.val().trim();
-    var auth = firebase.auth();
+        var email = txtEmail.val().trim();
+        var pass = txtPassword.val().trim();
+        var auth = firebase.auth();
 
-    // Sign In
-    var promise = firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
-        // Handle Errors here.
+        // Sign In
+        var promise = firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+            // Handle Errors here.
 
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        $('#user-message').append(errorMessage);
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            $('#user-message').append(errorMessage);
 
 
+
+        });
 
     });
 
-});
 
 
+    btnSignUp.on('click', function() {
 
-btnSignUp.on('click', function() {
+        //get email and pass
+        var email = txtEmail.val().trim();
+        var pass = txtPassword.val().trim();
+        var auth = firebase.auth();
 
-    //get email and pass
-    var email = txtEmail.val().trim();
-    var pass = txtPassword.val().trim();
-    var auth = firebase.auth();
+        // Sign In
+        var promise = firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
 
-    // Sign In
-    var promise = firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            $('#user-message').html(errorCode);
+            $('#user-message').html(errorMessage);
 
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        $('#user-message').html(errorCode);
-        $('#user-message').html(errorMessage);
+        });
 
     });
-    txtEmail.val("");
-    txtPassword.val("");
 
-});
+    btnLogout.on('click', function() {
 
-btnLogout.on('click', function() {
-
-    firebase.auth().signOut();
-    $('#user-message').html(' You have signed out.');
-});
+        firebase.auth().signOut();
+        $('#user-message').html(' You have signed out.');
+    });
 
 
-//Add real-time authentication listener
+    //Add real-time authentication listener
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-        btnLogin.hide();
-        btnSignUp.hide();
-        btnLogout.show();
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            btnLogin.hide();
+            btnSignUp.hide();
+            btnLogout.show();
 
-        console.log(user);
-        $('#user-message').html('You are signed in. ');
+            console.log(user);
+            $('#user-message').html('You are signed in. ');
+            writeDatabase();
+            txtEmail.val("");
+            txtPassword.val("");
 
+        } else {
+            $('#user-message').html("You are not signed in. ");
+            btnLogin.show();
+            btnSignUp.show();
+            btnLogout.hide();
+        }
+    });
+
+    function writeDatabase() {
         var email = txtEmail.val().trim();
 
         database.child('users').push({
@@ -127,14 +138,5 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 
         });
-
-        txtEmail.val("");
-        txtPassword.val("");
-
-    } else {
-        $('#user-message').html("You are not signed in. ");
-        btnLogin.show();
-        btnSignUp.show();
-        btnLogout.hide();
     }
 });
